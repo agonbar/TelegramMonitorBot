0b9b07fda82557c07ef77b2ea48f8e82267fb5d8
Adrian Gonzalez Barbosa <adrian.gonzalez.barbosa@gmail.com>
2016-11-10 19:47:01 +0100

    Formated disks

--- index.js
+++ index.js
@@ -25,15 +25,14 @@ bot.onText(/\/state(.*)/, function(msg, match) {
 		var ramUsed = (memLoad.free/1073741824).toFixed(2);
 		var ramTotal = (memLoad.total/1073741824).toFixed(2);
 		var temp = ''
-		if(temps.max !== -1) { temp = "Temps->"+temps.cores }
-		console.log(temps);
+		if(temps.max !== -1) { temp = "Temps->"+temps.cores+"\n" }
        		bot.sendMessage(msg.chat.id,
 			"===="+osInfo.hostname+"====\n"+
 			"Cpu ->"+cpuLoad.currentload.toFixed(2)+"%\n"+
 			"Ram->"+ramUsed+"/"+ramTotal+"Gb\n"+
 			"Disk->"+fsSpace.filter(function(x){return x.mount=="/"})[0].use+"%\n"+
 			"IP->"+ip+"\n"+
-			temp+"\n"+
+			temp+
 			"=============="
 		);
 	});});});});});});
@@ -47,6 +46,22 @@ bot.onText(/\/dockers(.*)/, function(msg, match) {
 			response = response+"===="+containers[i].name+"====\n"+
 			"Image: "+containers[i].image+"\n"+
 			"State: "+containers[i].state+"\n"
+		};
+		response = response+"============";
+                bot.sendMessage(msg.chat.id, response);
+        });
+});
+
+bot.onText(/\/disks(.*)/, function(msg, match) {
+
+        si.fsSize(function(fsSpace) {
+		var response = "";
+		filteredFs = fsSpace.filter(function(x){return x.fs.match(/^((?!loop).)*$/gi)});
+		for (i in filteredFs) {
+			response = response+"===="+filteredFs[i].fs+"====\n"+
+			"Mount: "+filteredFs[i].mount+"\n"+
+			"Total: "+(filteredFs[i].size/1073741824).toFixed(2)+"GB\n"+
+			"Use: "+filteredFs[i].use+"%\n"
 		};
 		response = response+"============";
                 bot.sendMessage(msg.chat.id, response);
